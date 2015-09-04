aa4d902b045dfc810b9ba60cc9b4e902ae5dc0d4
unknown <dorian.lopez@desarrollo1.sigmamovil.local>
2015-09-04 17:54:39 -0500

    no message

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -7,10 +7,6 @@ class IndexController extends ControllerBase
         $query = $this->modelsManager->createQuery("SELECT Buy.*, Article.* FROM Buy JOIN Article WHERE Buy.idUser = {$this->user->idUser}");
         $buys = $query->execute();
         
-        foreach ($buys as $b) {
-            $this->logger->log("x: {$b->buy->value}");
-        }
-        
         $this->view->setVar("user", $this->user);
         $this->view->setVar("buys", $buys);
     }
