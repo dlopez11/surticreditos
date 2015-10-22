f1b49ee894ab9453171fda9babe0abf26e820ee6
unknown <dorian.lopez@desarrollo1.sigmamovil.local>
2015-10-22 14:51:21 -0500

    no message

--- app/controllers/ImportdataController.php
+++ app/controllers/ImportdataController.php
@@ -9,74 +9,67 @@ class ImportdataController extends ControllerBase
     
     public function importfileoneAction()
     {
-        if($this->request->isPost()){  
-            try {  
+        if($this->request->isPost()){
+            try {
                 $update = $this->request->getPost("update");
-                $update = ($update == 'on' ? 1 : 0);     
+                $update = ($update == 'on' ? 1 : 0);
                 
-                if ($_FILES['csvone']['size'] > 3145728){
-                    return $this->set_json_response(array('El archivo CSV no puede ser mayor a 3 MB de peso'), 403);
+                $file = $_FILES['csvone'];
+                
+                $validateFile = new \Surticreditos\Misc\ValidateFile();
+                $validateFile->setFile($file);
+                $validateFile->validate();
+
+                $csv = $file['tmp_name'];
+                $handle = fopen($csv,'r');
+
+                $values = array();
+                $txt = array();
+
+                while($data = fgetcsv($handle,1000,";","'")){
+                    if($data[0]){
+                        $values[] = "$data[0]";
+                    }
+                }                                
+
+                foreach ($values as $key => $value) {
+                    $c = substr($value, 0, 11);
+                    $n = substr($value, 11, 40);
+                    $cl = substr($value, 51, 1);
+                    $d = substr($value, 52, 30);
+                    $t = substr($value, 82, 7);
+                    $cel = substr($value, 90, 11);
+                    $e = substr($value, 102, 59);                    
+                    $ci = substr($value, 163, 5);
+
+                    $ce = ltrim($c,'0');
+
+                    $id = trim($ce);
+                    $name = trim($n);
+                    $class = trim($cl);
+                    $address = trim($d);
+                    $phone = trim($t);
+                    $celphone = trim($cel);
+                    $email = trim($e);
+                    $city = trim($ci);
+
+                    if(!empty($ce)){
+                        $txt[] = "($id,2," . time() . "," . time() . ",0,'$id','$name','$class','$address','$phone - $celphone','$email','$city')";
+                        $text = implode(", ", $txt);
+                    }                    
                 }
 
-                if ($_FILES['csvone']['size'] > 0) {
+                if(!$update){
+                    $sql = "INSERT IGNORE INTO user (idUser, idRole, created, updated, status, password, name, class, address, phone, email, city) VALUES {$text}";                    
+                }  
+                else {
+                    $sql = "INSERT IGNORE INTO user (idUser, idRole, created, updated, status, password, name, class, address, phone, email, city) VALUES {$text} ON DUPLICATE KEY UPDATE updated = VALUES(updated), name = VALUES(name), class = VALUES(class), address = VALUES(address), phone = VALUES(phone), email = VALUES(email), city = VALUES(city)";                   
+                } 
 
-                    $fileinfo = pathinfo($_FILES['csvone']['name']);
+                $result = $this->db->execute($sql);
 
-                    if(strtolower(trim($fileinfo["extension"])) != "csv")
-                    {
-                        return $this->set_json_response(array('Por favor seleccione un archivo de tipo CSV'), 403);
-                    }
-
-                    $csv = $_FILES['csvone']['tmp_name'];
-                    $handle = fopen($csv,'r');
-
-                    $values = array();
-                    $txt = array();
-
-                    while($data = fgetcsv($handle,1000,";","'")){
-                        if($data[0]){
-                            $values[] = "$data[0]";
-                        }
-                    }                                
-
-                    foreach ($values as $key => $value) {
-                        $c = substr($value, 0, 11);
-                        $n = substr($value, 11, 40);
-                        $cl = substr($value, 51, 1);
-                        $d = substr($value, 52, 30);
-                        $t = substr($value, 82, 7);
-                        $cel = substr($value, 90, 11);
-                        $e = substr($value, 102, 59);                    
-                        $ci = substr($value, 163, 5);
-
-                        $ce = ltrim($c,'0');
-
-                        $id = trim($ce);
-                        $name = trim($n);
-                        $class = trim($cl);
-                        $address = trim($d);
-                        $phone = trim($t);
-                        $celphone = trim($cel);
-                        $email = trim($e);
-                        $city = trim($ci);
-
-                        if(!empty($ce)){
-                            $txt[] = "($id,2," . time() . "," . time() . ",0,'$id','$name','$class','$address','$phone - $celphone','$email','$city')";
-                            $text = implode(", ", $txt);
-                        }                    
-                    }
-
-                    if(!$update){
-                        $sql = "INSERT IGNORE INTO user (idUser, idRole, created, updated, status, password, name, class, address, phone, email, city) VALUES {$text}";                    
-                    }  
-                    else {
-                        $sql = "INSERT IGNORE INTO user (idUser, idRole, created, updated, status, password, name, class, address, phone, email, city) VALUES {$text} ON DUPLICATE KEY UPDATE updated = VALUES(updated), name = VALUES(name), class = VALUES(class), address = VALUES(address), phone = VALUES(phone), email = VALUES(email), city = VALUES(city)";                   
-                    } 
-
-                    $result = $this->db->execute($sql);
-                    
-                    return $this->set_json_response(array('El archivo se importo exitosamente'), 200);                                               
-                }
+                return $this->set_json_response(array('El archivo se importo exitosamente'), 200);                                               
+                
             }
             catch(Exception $e) {
                 $this->logger->log("Exception while inserting users: {$e->getMessage()}");
@@ -88,79 +81,25 @@ class ImportdataController extends ControllerBase
     public function importfiletwoAction()
     {
         try {
-            if ($_FILES['csvtwo']['size'] > 3145728){
-                return $this->set_json_response(array('El archivo CSV no puede ser mayor a 3 MB de peso'), 403);
-            }
+            $file = $_FILES['csvtwo'];
             
-            if ($_FILES['csvtwo']['size'] > 0) {
-
-                $fileinfo = pathinfo($_FILES['csvtwo']['name']);
-                
-                if(strtolower(trim($fileinfo["extension"])) != "csv")
-                {
-                    return $this->set_json_response(array('Por favor seleccione un archivo de tipo CSV'), 403);
-                }
-                   
-                $csv = $_FILES['csvtwo']['tmp_name'];
-                $handle = fopen($csv,'r');
-                
-                $values = array();
-                $text = array();
-                
-                while($data = fgetcsv($handle,1000,";","'")){
-                    if($data[0]){
-                        $values[] = "$data[0]";
-                    }
-                }
-                
-                foreach ($values as $key => $value) {
-                    $c = substr($value, 0, 11);
-                    $cu = substr($value, 11, 7);
-                    $v = substr($value, 18, 10);
-                    $aa = substr($value, 28, 4);
-                    $mm = substr($value, 32, 2);
-                    $dd = substr($value, 34, 2);
-                    $s = substr($value, 36, 10);
-                    
-                    $ced = ltrim($c,'0');
-                    $cue = ltrim($cu,'0');
-                    $val = ltrim($v,'0');
-                    $sal = ltrim($s,'0');
-                    
-                    $cedula = trim($ced);
-                    $cuenta = trim($cue);
-                    $valor = trim($val);
-                    $año = trim($aa);
-                    $mes = trim($mm);
-                    $dia = trim($dd);
-                    $saldo = trim($sal);
-                    
-                    $fecha = "$año-$mes-$dia";
-                    
-                    $txt[] = "($cuenta,$cedula,'$fecha',$valor,$saldo)";
-                    $text = implode(", ", $txt);
-                                        
-                }
-                
-                $sql1 = "SET FOREIGN_KEY_CHECKS = 0";
-                $result1 = $this->db->execute($sql1);
-                
-                $sqlremove = "TRUNCATE TABLE buy";
-                $resultremove = $this->db->execute($sqlremove);
-                
-                $sql = "INSERT IGNORE INTO buy (idBuy, idUser, date, value, debt) VALUES {$text} ON DUPLICATE KEY UPDATE date = VALUES(date), value = VALUES(value), debt = VALUES(debt)";
-                $result = $this->db->execute($sql);
-                
-                $sql2 = "SET FOREIGN_KEY_CHECKS = 1";
-                $result1 = $this->db->execute($sql2);
-
-                return $this->set_json_response(array('El archivo se importo exitosamente'), 200);                                               
-            }
+            $validateFile = new \Surticreditos\Misc\ValidateFile();
+            $validateFile->setFile($file); 
+            $validateFile->validate(); 
+            $validateFile->formatFileForBuy(); 
+            $validateFile->generateFinalFile();
+            $validateFile->loadDataOnDb("buy");
+             
+            return $this->set_json_response(array('El archivo se importo exitosamente'), 200);
+        }
+        catch(InvalidArgumentException $e) {
+            $this->logger->log("Exception while inserting buys: {$e->getMessage()}");
+            return $this->set_json_response(array($e->getMessage()), 404);
         }
         catch(Exception $e) {
             $this->logger->log("Exception while inserting buys: {$e->getMessage()}");
-            return $this->set_json_response(array("Ha ocurrido un error, por favor contacte al administrador"), 403);
-        }        
+            return $this->set_json_response(array("Ha ocurrido un error, por favor contacte al administrador"), 500);
+        }                                                
     }
     
     public function importfilethreeAction()
@@ -189,77 +128,25 @@ class ImportdataController extends ControllerBase
     
     public function importfilefourAction()
     {
-         try {
-            if ($_FILES['csvfour']['size'] > 3145728){
-                return $this->set_json_response(array('El archivo CSV no puede ser mayor a 3 MB de peso'), 403);
-            }
+        try {
+            $file = $_FILES['csvfour'];
             
-            if ($_FILES['csvfour']['size'] > 0) {
-
-                $fileinfo = pathinfo($_FILES['csvfour']['name']);
-                
-                if(strtolower(trim($fileinfo["extension"])) != "csv")
-                {
-                    return $this->set_json_response(array('Por favor seleccione un archivo de tipo CSV'), 403);
-                }
-                   
-                $csv = $_FILES['csvfour']['tmp_name'];
-                $handle = fopen($csv,'r');
-                
-                $values = array();
-                $text = array();
-                
-                while($data = fgetcsv($handle,1000,";","'")){
-                    if($data[0]){
-                        $values[] = "$data[0]";
-                    }
-                }
-                
-                foreach ($values as $key => $value) {
-                    $cu = substr($value, 0, 7);
-                    $re = substr($value, 7, 14);
-                    $no = substr($value, 21, 20);
-                    $ca = substr($value, 41, 5);                   
-                    
-                    $cue = ltrim($cu,'0');
-                    $ref = ltrim($re,'0');
-                    $nom = ltrim($no,'0');
-                    $can = ltrim($ca,'0');                                        
-                                        
-                    $cuenta = trim($cue);                    
-                    $referencia = trim($ref);
-                    $nombre = trim($nom);
-                    $cantidad = trim($can);
-                    
-                    $caracteres = array('"',"'");
-                    $replace = array("p","p");
-                    
-                    $name = str_replace($caracteres, $replace, $nombre);
-                    $reference = str_replace($caracteres, $replace, $referencia);
-                    
-                    $txt[] = "(null,$cuenta,'$reference','$name',$cantidad)";
-                    $text = implode(", ", $txt);
-                                        
-                }                                
-                
-                $sql1 = "SET FOREIGN_KEY_CHECKS = 0";
-                $result1 = $this->db->execute($sql1);
-                
-                $sqlremove = "TRUNCATE TABLE article";
-                $resultremove = $this->db->execute($sqlremove);
-                
-                $sql = "INSERT IGNORE INTO article (idArticle, idBuy, reference, name, quantity) VALUES {$text}";
-                $result = $this->db->execute($sql);
-                
-                $sql2 = "SET FOREIGN_KEY_CHECKS = 1";
-                $result2 = $this->db->execute($sql2);
-
-                return $this->set_json_response(array('El archivo se importo exitosamente'), 200);                                               
-            }
+            $validateFile = new \Surticreditos\Misc\ValidateFile();
+            $validateFile->setFile($file); 
+            $validateFile->validate(); 
+            $validateFile->formatFileForArticle(); 
+            $validateFile->generateFinalFile();
+            $validateFile->loadDataOnDb("article");
+             
+            return $this->set_json_response(array('El archivo se importo exitosamente'), 200);
+        }
+        catch(InvalidArgumentException $e) {
+            $this->logger->log("Exception while inserting buys: {$e->getMessage()}");
+            return $this->set_json_response(array($e->getMessage()), 404);
         }
         catch(Exception $e) {
             $this->logger->log("Exception while inserting buys: {$e->getMessage()}");
-            return $this->set_json_response(array("Ha ocurrido un error, por favor contacte al administrador"), 403);
-        }
-    }        
+            return $this->set_json_response(array("Ha ocurrido un error, por favor contacte al administrador"), 500);
+        }                                         
+    }
 }
--- app/misc/ValidateFile.php
+++ app/misc/ValidateFile.php
@@ -39,7 +39,7 @@ class ValidateFile
         }
     }
     
-    public function formatFileForPayment()
+    public function insertDataFromFileToArray()
     {
         $csv = $this->file['tmp_name'];
         $handle = \fopen($csv,'r');
@@ -50,9 +50,15 @@ class ValidateFile
             if($data[0]){
                 $values[] = "$data[0]";
             }
-        }                
+        }
+        
+        return $values;
+    }
 
-        foreach ($values as $key => $value) {
+
+    public function formatFileForPayment()
+    {                
+        foreach ($this->insertDataFromFileToArray() as $key => $value) {
             $ce = \substr($value, 0, 11);
             $cu = \substr($value, 11, 7);
             $r = \substr($value, 18, 7);
@@ -87,6 +93,68 @@ class ValidateFile
         $this->txt = $txt;
     }
     
+    public function formatFileForBuy()
+    {        
+        foreach ($this->insertDataFromFileToArray() as $key => $value) {
+            $c = \substr($value, 0, 11);
+            $cu = \substr($value, 11, 7);
+            $v = \substr($value, 18, 10);
+            $aa = \substr($value, 28, 4);
+            $mm = \substr($value, 32, 2);
+            $dd = \substr($value, 34, 2);
+            $s = \substr($value, 36, 10);
+
+            $ced = \ltrim($c,'0');
+            $cue = \ltrim($cu,'0');
+            $val = \ltrim($v,'0');
+            $sal = \ltrim($s,'0');
+
+            $cedula = \trim($ced);
+            $cuenta = \trim($cue);
+            $valor = \trim($val);
+            $año = \trim($aa);
+            $mes = \trim($mm);
+            $dia = \trim($dd);
+            $saldo = \trim($sal);
+
+            $fecha = "$año-$mes-$dia";
+
+            $txt[] = "$cuenta,$cedula,$fecha,$valor,$saldo";
+        }
+        
+        $this->txt = $txt;
+    }
+    
+    public function formatFileForArticle()
+    {        
+        foreach ($this->insertDataFromFileToArray() as $key => $value) {
+            $cu = \substr($value, 0, 7);
+            $re = \substr($value, 7, 14);
+            $no = \substr($value, 21, 20);
+            $ca = \substr($value, 41, 5);                   
+
+            $cue = \ltrim($cu,'0');
+            $ref = \ltrim($re,'0');
+            $nom = \ltrim($no,'0');
+            $can = \ltrim($ca,'0');                                        
+
+            $cuenta = \trim($cue);                    
+            $referencia = \trim($ref);
+            $nombre = \trim($nom);
+            $cantidad = \trim($can);
+
+            $caracteres = array('"',"'");
+            $replace = array("p","p");
+
+            $name = \str_replace($caracteres, $replace, $nombre);
+            $reference = \str_replace($caracteres, $replace, $referencia);
+
+            $txt[] = "null,$cuenta,$reference,$name,$cantidad";
+        }
+        
+        $this->txt = $txt;
+    }
+
     public function generateFinalFile()
     {
         $filename = $this->path->path . $this->path->tmpfolder . \uniqid() . \time() . ".csv";
@@ -114,11 +182,21 @@ class ValidateFile
         if ($table == "payment") {
             $fields = " (idPayment, idBuy, receiptValue, date)";
         }
-        
+        else if($table == "buy") {
+            $fields = " (idBuy, idUser, date, value, debt)";
+        }
+        else if ($table == "article") {
+            $fields = " (idArticle, idBuy, reference, name, quantity)";
+        }
+                
         return $fields;
     }
     
     private function load($table, $fields) {
+        
+        if ($fields == NULL) {
+            throw new \Exception('Fields is empty ' . $table);
+        }
         $sql_db_mode = "SET session sql_mode=''";
                 
         $importfile = "LOAD DATA INFILE '{$this->filename}' IGNORE INTO TABLE {$table} CHARACTER SET UTF8 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'"
